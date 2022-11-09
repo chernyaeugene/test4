984f85aaaba08729dff126dd3aee8668e48fbe0d
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-11-09 17:01:28 +0200

    learn

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+    const [users, setUsers] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((res) => res.json())
+            .then((json) => {
+                setUsers(json.data)
+            })
+            .catch((err) => {
+                console.warn(err);
+                alert('Error when getting Users data')
+            })
+            .finally(() => setIsLoading(false))
+    }, [])
   return (
       <div className="App">
-        {/*<Users />*/}
-         <Success />
+        <Users items={users} isLoading={isLoading}/>
+        {/* <Success />*/}
       </div>
   );
 }
